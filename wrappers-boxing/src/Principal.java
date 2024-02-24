685d615271fa46184411191e39046304402e3f6e
Marcelo <marceloo.cfreitas@gmail.com>
2024-02-24 15:30:33 -0400

    Refatorando para a forma mais simplificada

--- wrappers-boxing/src/Principal.java
+++ wrappers-boxing/src/Principal.java
@@ -2,8 +2,12 @@ public class Principal {
 
     public static void main(String[] args) {
 
+//        Cliente cliente = new Cliente();
+//        cliente.idade = Integer.valueOf(20);
+
+        //Forma simplificada a partir do Java 5
         Cliente cliente = new Cliente();
-        cliente.idade = Integer.valueOf(20);
+        cliente.idade = 20;
 
         System.out.printf("Idade: %d%n", cliente.idade);
 
