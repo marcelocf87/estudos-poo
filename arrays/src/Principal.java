91a39dfb4b1be0cd7613a0ba65e5723956cec6cc
Marcelo <marceloo.cfreitas@gmail.com>
2024-02-26 16:13:30 -0400

    Iterando arrays de objetos

--- arrays/src/Principal.java
+++ arrays/src/Principal.java
@@ -17,6 +17,17 @@ public class Principal {
 
         turmaB.alunos[1] = aluno1;
 
+        for (int i = 0; i < turmaB.alunos.length; i++) {
+            Aluno aluno = turmaB.alunos[i];
+
+            if (aluno != null) {
+                System.out.printf("%d - %s (%d anos)%n", i, aluno.nome, aluno.idade);
+            } else {
+                System.out.printf("%d - Vago%n", i);
+            }
+
+        }
+
 
     }
 }
